8d10cf0d1442e876bf8b078921bf89b01edf6b52
Shruti Gupta <shrutigupta5555@gmail.com>
2022-11-14 23:48:30 +0530

    maps

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:treebee/main.dart';
 import 'package:treebee/screens/new_tree.dart';
 import 'package:treebee/screens/tree_profile.dart';
@@ -14,6 +19,76 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _controller = Completer();
+  // Position? _currentPosition = Position(longitude: 37.42796133580664, latitude:  -122.085749655962);
+  Position? _currentPosition = Position(
+      longitude: 0,
+      latitude: 0,
+      timestamp: DateTime.now(),
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
+  @override
+  void initState() {
+    _getCurrentPosition();
+    getAdoptedTrees();
+  }
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      print(position);
+      setState(() => _currentPosition = position);
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          bearing: 192.8334901395799,
+          target: LatLng(position.latitude, position.longitude),
+          tilt: 59.440717697143555,
+          zoom: 19.151926040649414)));
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+  void getAdoptedTrees() async {
+    var x = await FirebaseFirestore.instance.collection("trees").get();
+    var y = List.from(x.docs);
+    print(y);
+  }
+
   var adoptedTrees = [
     {
       "id": "123",
@@ -96,7 +171,17 @@ class _HomeState extends State<Home> {
               Container(
                 height: 2 / 3 * h,
                 width: w - 40,
-                color: Colors.pink,
+                color: Colors.white,
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(_currentPosition!.latitude,
+                        _currentPosition!.longitude),
+                    zoom: 14.4746,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
               ),
               SizedBox(
                 height: 15,
